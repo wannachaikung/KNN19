9fe90fa98ef21f0bb35fb86082aa27a25170144f
wannachaikung <wannachai200412@gmail.com>
2025-11-19 15:32:21 +0700

    .

--- Home.py
+++ Home.py
@@ -4,5 +4,19 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-st.header("Wannachai")
+st.header("โปรเจคการจำแนกข้อมูลดอกไม้ Iris ด้วย KNN")
 st.image("./img/wannachai.jpg")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("Versicolor")
+   st.image("./img/iris1.jpg")
+
+with col2:
+   st.header("Verginiga")
+   st.image("./img/iris2.jpg")
+
+with col3:
+   st.header("Setosa")
+   st.image("./img/iris3.jpg")
