26c36b3fd0ac386f2f74eea0c0701971d1030fd3
wannachaikung <wannachai200412@gmail.com>
2025-11-19 15:54:59 +0700

    .

--- Home.py
+++ Home.py
@@ -60,3 +60,24 @@ pt_wd = st.slider("กรุณาเลือกข้อมูล petal.width"
 
 sp_len = st.number_input("กรุณาเลือกข้อมูล sepal.length")
 sp_wd = st.number_input("กรุณาเลือกข้อมูล sepal.width")
+
+if st.button("ทำนายผล"):
+    #st.write("ทำนาย")
+   dt = pd.read_csv("./data/iris.csv") 
+   X = dt.drop('variety', axis=1)
+   y = dt.variety   
+   Knn_model = KNeighborsClassifier(n_neighbors=3)
+   Knn_model.fit(X, y)   
+   x_input = np.array([[pt_len, pt_wd, sp_len, sp_wd]])
+   st.write(Knn_model.predict(x_input))
+
+   out=Knn_model.predict(x_input)
+
+   if out[0] == 'Setosa':
+    st.image("./pic/iris1.jpg")
+   elif out[0] == 'Versicolor':       
+    st.image("./pic/iris2.jpg")
+   else:
+    st.image("./pic/iris3.jpg")
+else:
+    st.write("ไม่ทำนาย")
