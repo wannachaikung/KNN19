07bf3e1e2ec38de953660e6f5035d6139b9234d5
wannachaikung <wannachai200412@gmail.com>
2025-11-19 15:26:31 +0700

    edit home

--- Home.py
+++ Home.py
@@ -3,8 +3,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import plotly.express as px
-import plotly.graph_objects as go
 
 st.header("Wannachai")
 st.image('.img/wannachai.jpg')
